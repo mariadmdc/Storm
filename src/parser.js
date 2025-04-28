0afcd33928aedd79af9987fc667c828ddd77083a
GavinButts <97565155+GavinButts@users.noreply.github.com>
2025-04-28 13:49:32 -0700

    minor updates

--- src/parser.js
+++ src/parser.js
@@ -1,14 +1,17 @@
-// The parse() function uses Ohm to produce a match object for a given
-// source code program, using the grammar in the file carlos.ohm.
+import * as fs    from "node:fs";
+import * as ohm   from "ohm-js";
+import analyze    from "./analyzer.js";
 
-import * as fs from "node:fs"
-import * as ohm from "ohm-js"
+const grammar = ohm.grammar(
+  fs.readFileSync("src/storm.ohm", "utf8")
+);
 
-const grammar = ohm.grammar(fs.readFileSync("src/carlos.ohm"))
-
-// Returns the Ohm match if successful, otherwise throws an error
 export default function parse(sourceCode) {
-  const match = grammar.match(sourceCode)
-  if (!match.succeeded()) throw new Error(match.message)
-  return match
+  const match = grammar.match(sourceCode, "Program");
+  if (!match.succeeded()) {
+    // include line/col info from Ohm’s match.message
+    throw new SyntaxError(match.message);
+  }
+  // now run your semantics → AST / core IR
+  return analyze(match);
 }
